1f3b990838da34ae4bd6ec682c18a64967ad1780
Diego Illescas <dillescasl1800@alumno.ipn.mx>
2024-06-22 13:03:33 -0600

    Connection to the DB

--- app.js
+++ app.js
@@ -3,6 +3,25 @@ let path = require('path');
 let logger = require('morgan');
 let bodyParser = require('body-parser');
 let neo4j = require('neo4j-driver');
+require('dotenv').config();
+
+(async () => {
+    const URI = 'neo4j://localhost';
+    const USER = 'neo4j';
+    const PASSWORD = process.env.DB_PASSWORD;
+    let driver;
+
+    try{
+        driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD));
+        const serverInfo = await driver.getServerInfo();
+        console.log('Connection to the DB estabilished');
+        //console.log(serverInfo);
+    }catch(err){
+        console.log(`Connection error\n${err}\nCause: ${err.cause}`);
+        await driver.close()
+        return;
+    }
+})();
 
 var app = express();
 
